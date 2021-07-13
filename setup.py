dc73d4e87fefc40cae7cfdb69c86eecae0b8cd58
grburgess <jmichaelburgess@gmail.com>
2021-07-13 15:40:47 +0200

    updating to new install

--- setup.py
+++ setup.py
@@ -1,90 +1,7 @@
-from setuptools.command.build_ext import build_ext as _build_ext
-from Cython.Build import cythonize
-from setuptools import setup, find_packages, Command, Extension
+from setuptools import setup
 import os
-import io
-import sys
-from shutil import rmtree
 
-
-# Package meta-data.
-NAME = "ishockpy"
-DESCRIPTION = "An internal shock simulator"
-URL = "https://github.com/grburgess/ishockpy"
-EMAIL = "jmichaelburgess@gmail.com"
-AUTHOR = "J. Michael Burgess"
-REQUIRES_PYTHON = ">=2.7.0"
-VERSION = None
-
-REQUIRED = [
-    "numpy",
-    "scipy",
-    "ipython",
-    "h5py",
-    "pandas",
-    "ipywidgets",
-    "cython",
-    "numba"
-]
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-try:
-    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-        long_description = "\n" + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(os.path.join(here, NAME, "__version__.py")) as f:
-        exec(f.read(), about)
-else:
-    about["__version__"] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = "Build and publish the package."
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print("\033[1m{0}\033[0m".format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status("Removing previous builds...")
-            rmtree(os.path.join(here, "dist"))
-        except OSError:
-            pass
-
-        self.status("Building Source and Wheel (universal) distribution...")
-        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
-
-        self.status("Uploading the package to PyPI via Twine...")
-        os.system("twine upload dist/*")
-
-        self.status("Pushing git tags...")
-        os.system("git tag v{0}".format(about["__version__"]))
-        os.system("git push --tags")
-
-        sys.exit()
+import versioneer
 
 
 # Create list of data files
@@ -101,36 +18,11 @@ def find_data_files(directory):
     return paths
 
 
-#extra_files = find_data_files("popsynth/data")
-
+# extra_files = find_data_files("ishockpy/data")
 
 setup(
-    name=NAME,
-    version=about["__version__"],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=("tests",)),
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
- #   package_data={"": extra_files},
-    license="BSD",
-    cmdclass={"upload": UploadCommand},
+    version=versioneer.get_version(),
+    include_package_data=False,
+#    package_data={"": extra_files},
+    cmdclass=versioneer.get_cmdclass(),
 )
-
-
-from distutils.core import setup, Extension
-
-
-setup(name='ishockpy',
-      version='0.1',
-      description='an internal shock simulation',
-      author='J. Michael Burgess',
-      author_email='jmichaelburgess@gmail.com',
-      packages=['ishockpy'],
-     )
